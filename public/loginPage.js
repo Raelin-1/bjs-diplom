977851c3606fa109df99d6c26af0d1163b1f8e12
Raelin-1 <93232014+Raelin-1@users.noreply.github.com>
2024-07-26 23:21:05 +0500

    Add files via upload

---
+++ public/loginPage.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const user = new UserForm();
+user.loginFormCallback = function (data) {
+
+  ApiConnector.login(data, (res) => {
+    if (res.success) {
+      location.reload();
+    } else {
+      this.setLoginErrorMessage(res.error);
+    }
+  });
+};
+
+user.registerFormCallback = function (data) {
+  console.log(data);
+  ApiConnector.register(data, (res) => {
+    if (res.success) {
+      location.reload();
+    } else {
+      this.setMessage(res.succes, res.error);
+    }
+  });
+};
